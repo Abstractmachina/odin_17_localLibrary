1a96c65b5203e3d82f74c3728b9ce73cba19ed31
tc1421 <tc1421@ic.ac.uk>
2023-03-11 20:23:43 +0100

    added env variable

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +13,7 @@ var app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const mongoDB = "mongodb+srv://admin:RILx93GFGsH5npOb@cluster0.uipgrck.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_AUTH;
 
 main().catch(err => console.log(err));
 async function main() {
