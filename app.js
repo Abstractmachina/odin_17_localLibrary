ed614ff6908815d9ccf4e02791ea79744e527f6f
tc1421 <tc1421@ic.ac.uk>
2023-03-19 10:09:21 +0100

    added middleware for production

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 
+const compression = require('compression');
+const helmet = require('helmet');
+
 var app = express();
+app.use(compression());
+app.use(helmet());
+
+
+//set up rate limit
+var RateLimit = require('express-rate-limit');
+var limiter = RateLimit({
+  windowMs: 1*60*1000,
+  max:20,
+});
+app.use(limiter);
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
